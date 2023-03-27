795381b251912c5e484cff92fbdb9f40d7f13929
David Cavazos <dcavazos@google.com>
2023-03-27 14:20:54 -0700

    support higher-rank polymorphism (#23)
    * support higher-rank polymorphism
    
    * separate linter from tester
    
    * fix linter cache
    
    * break up Case into Or and Err

--- src/Core.hs
+++ src/Core.hs
@@ -7,25 +7,43 @@ import Data.Foldable (Foldable (foldl'))
 import Data.List (intercalate)
 
 {- TODO:
-* Function / operator overloading
-* Do notation
-  - Overload (<-) operator
+
+Clean up code
+- Push and pop variables from scope (fresh variables)
+- Show Expr with precedence
+
+Exhaustive pattern checks (warnings, not errors)
+- Use `Or` to express unions
+- Find all available cases
+  - Usually all the constructors on a union type
+  - Can be filtered if a type says a constructor is not possible (e.g. `head`)
+- Filter by all the `Case`s covered
+
+Function / operator overloading
+- Via inferred type classes
+
+Do notation
+- Overload (<-) operator
+
 -}
 
 data Expr
   = Typ
   | IntT
-  | Int !Int
   | NumT
+  | Int !Int
   | Num !Double
   | Ctr !String
   | Var !String
   | Let !Env !Expr
+  | For !String !Expr
   | Lam !String !Expr
+  | Case !String !Expr
   | Fun !Type !Type
+  | Or !Expr !Expr
   | App !Expr !Expr
-  | Case ![(String, Expr)]
   | Op !String ![Expr]
+  | Err
   deriving (Eq)
 
 showParen' :: Bool -> String -> String
@@ -42,10 +60,10 @@ showInfixR :: Int -> Int -> Expr -> String -> Expr -> String
 showInfixR p q a op b = showParen' (p > q) (showPrec (q + 1) a ++ op ++ showPrec q b)
 
 showPrec :: Int -> Expr -> String
-showPrec _ Typ = "@Type"
-showPrec _ IntT = "@Int"
+showPrec _ Typ = "%Type"
+showPrec _ IntT = "%Int"
 showPrec _ (Int i) = show i
-showPrec _ NumT = "@Num"
+showPrec _ NumT = "%Num"
 showPrec _ (Num n) = show n
 showPrec _ (Ctr k) = "#" ++ k
 showPrec _ (Var x) = x
@@ -53,13 +71,14 @@ showPrec _ (Let env b) = do
   let showDef (x, a) = x ++ " = " ++ show a
   let x = "[" ++ intercalate "; " (map showDef env) ++ "]"
   show (App (Var x) b)
-showPrec p (Lam x a) = showPrefix p 1 ("\\" ++ x ++ " -> ") a
-showPrec p (Fun a b) = showInfixR p 1 a " -> " b
-showPrec p (App a b) = showInfixL p 2 a " " b
-showPrec _ (Case cases) = do
-  let showCase (k, b) = k ++ " -> " ++ show b
-  "{" ++ intercalate " | " (map showCase cases) ++ "}"
+showPrec p (For x a) = showPrefix p 2 ("@" ++ x ++ ". ") a
+showPrec p (Lam x a) = showPrefix p 2 ("\\" ++ x ++ " -> ") a
+showPrec p (Case k a) = showPrefix p 2 ("\\" ++ show (Ctr k) ++ " -> ") a
+showPrec p (Fun a b) = showInfixR p 2 a " -> " b
+showPrec p (Or a b) = showInfixL p 1 a " | " b
+showPrec p (App a b) = showInfixL p 3 a " " b
 showPrec _ (Op op args) = "%" ++ op ++ " (" ++ intercalate ", " (map show args) ++ ")"
+showPrec _ Err = "@error"
 
 instance Show Expr where
   show :: Expr -> String
@@ -71,12 +90,9 @@ type Ops = [(String, [Expr] -> Maybe Expr)]
 
 type Env = [(String, Expr)]
 
-data Scheme = For ![String] !Type
-  deriving (Eq, Show)
-
 data Symbol
   = Val !Expr
-  | Ann !Expr !Scheme
+  | Ann !Expr !Type
   deriving (Eq, Show)
 
 type Context = [(String, Symbol)]
@@ -85,12 +101,12 @@ data TypeError
   = InfiniteType !String !Expr
   | InvalidCtr !String !Symbol
   | InvalidOp !String !Symbol
-  | NotAFunction !Expr !Type
   | Mismatch !Expr !Expr
+  | NotAFunction !Expr !Type
+  | RuntimeError
   | UndefinedCtr !String
   | UndefinedOp !String
   | UndefinedVar !String
-  | EmptyCase
   deriving (Eq, Show)
 
 fun :: [Type] -> Type -> Type
@@ -108,7 +124,7 @@ pop x ((x', _) : kvs) | x == x' = kvs
 pop x (_ : kvs) = pop x kvs
 
 set :: Eq k => k -> v -> [(k, v)] -> [(k, v)]
-set x y [] = [(x, y)]
+set _ _ [] = []
 set x y ((x', _) : kvs) | x == x' = (x, y) : kvs
 set x y (kv : kvs) = kv : set x y kvs
 
@@ -124,12 +140,22 @@ eval ops env (Var x) = case lookup x env of
   Just a -> eval ops env a
   Nothing -> Var x
 eval ops env (Let env' a) = eval ops (env ++ env') a
+eval ops env (For x a) = For x (eval ops ((x, Var x) : env) a)
 eval ops env (Lam x a) = Lam x (eval ops ((x, Var x) : env) a)
+eval ops env (Case k a) = Case k (eval ops env a)
 eval ops env (Fun a b) = Fun (eval ops env a) (eval ops env b)
+eval _ _ (Or a b) = Or a b
 eval ops env (App a b) = case (eval ops env a, eval ops env b) of
-  (Lam x a, b) -> eval ops ((x, b) : env) a
+  (Lam x a, b) -> eval ops [(x, b)] a
+  (Case k a, b) -> case spine b of
+    (Var _ : _) -> App (Case k a) b
+    (Ctr k' : bs) | k == k' -> eval ops env (app a bs)
+    _patternMismatch -> Err
+  (Or a1 a2, b) -> case eval ops env (App a1 b) of
+    Err -> eval ops env (App a2 b)
+    c@App {} -> Or c (App a2 b)
+    c -> c
   (a, b) -> App a b
-eval ops env (Case cases) = Case (second (eval ops env) <$> cases)
 eval ops env (Op op args) = do
   let args' = eval ops env <$> args
   case lookup op ops of
@@ -137,6 +163,12 @@ eval ops env (Op op args) = do
       Just a -> a
       Nothing -> Op op args'
     Nothing -> Op op args'
+eval _ _ Err = Err
+
+spine :: Expr -> [Expr]
+spine (App a b) = spine a ++ [b]
+spine Err = []
+spine a = [a]
 
 ctxEnv :: Context -> Env
 ctxEnv [] = []
@@ -156,20 +188,20 @@ occurs _ (Ctr _) = False
 occurs x (Var y) = x == y
 occurs x (Let env _) | x `elem` map fst env = False
 occurs x (Let _ a) = occurs x a
+occurs x (For x' _) | x == x' = False
+occurs x (For _ a) = occurs x a
 occurs x (Lam x' _) | x == x' = False
 occurs x (Lam _ a) = occurs x a
+occurs x (Case _ a) = occurs x a
 occurs x (Fun a b) = occurs x a || occurs x b
+occurs x (Or a b) = occurs x a || occurs x b
 occurs x (App a b) = occurs x a || occurs x b
-occurs x (Case cases) = any (occurs x . snd) cases
 occurs x (Op _ args) = any (occurs x) args
+occurs _ Err = False
 
 apply :: Ops -> (String, Expr) -> Symbol -> Symbol
-apply ops sub (Val b) = Val (eval ops [sub] b)
-apply _ (x, _) (Ann b (For xs t)) | x `elem` xs = Ann b (For xs t)
-apply ops sub (Ann b (For xs t)) = do
-  let b' = eval ops [sub] b
-  let t' = eval ops (sub : map (\x -> (x, Var x)) xs) t
-  Ann b' (For xs t')
+apply ops sub (Val a) = Val (eval ops [sub] a)
+apply ops sub (Ann a t) = Ann (eval ops [sub] a) (eval ops [sub] t)
 
 unify :: Ops -> Context -> Expr -> Expr -> Either TypeError Context
 unify ops ctx a b = case (solve ops ctx a, solve ops ctx b) of
@@ -184,7 +216,16 @@ unify ops ctx a b = case (solve ops ctx a, solve ops ctx b) of
   (Var x, b) ->
     Right (set x (Val b) (map (second $ apply ops (x, b)) ctx))
   (a, Var y) -> unify ops ctx (Var y) a
+  (For x a, b) -> do
+    let xT = newName (x ++ "T") (map fst ctx)
+    ctx <- unify ops ((xT, Ann (Var xT) Typ) : (x, Ann (Var x) (Var xT)) : ctx) a b
+    Right (pop x ctx)
+  (a, For x b) -> do
+    let xT = newName (x ++ "T") (map fst ctx)
+    ctx <- unify ops ((xT, Ann (Var xT) Typ) : (x, Ann (Var x) (Var xT)) : ctx) a b
+    Right (pop x ctx)
   -- TODO: Lam
+  -- TODO: Case
   (Fun a1 a2, Fun b1 b2) -> do
     ctx <- unify ops ctx a1 b1
     unify ops ctx a2 b2
@@ -194,13 +235,12 @@ unify ops ctx a b = case (solve ops ctx a, solve ops ctx b) of
   -- TODO: Op
   (a, b) -> Left (Mismatch a b)
 
-instantiate :: Ops -> Context -> Scheme -> Either TypeError (Type, Context)
-instantiate ops ctx (For [] a) =
-  Right (solve ops ctx a, ctx)
-instantiate ops ctx (For (x : xs) a) = do
+instantiate :: Ops -> Context -> Type -> (Type, Context)
+instantiate ops ctx (For x a) = do
   let y = newName x (map fst ctx)
   let a' = eval ops [(x, Var y)] a
-  instantiate ops ((y, Val (Var y)) : ctx) (For xs a')
+  instantiate ops ((y, Val (Var y)) : ctx) a'
+instantiate ops ctx a = (solve ops ctx a, ctx)
 
 inferType :: Ops -> Context -> Expr -> Either TypeError (Type, Context)
 inferType _ ctx Typ = Right (Typ, ctx)
@@ -209,58 +249,72 @@ inferType _ ctx (Int _) = Right (IntT, ctx)
 inferType _ ctx NumT = Right (Typ, ctx)
 inferType _ ctx (Num _) = Right (NumT, ctx)
 inferType ops ctx (Ctr k) = case lookup k ctx of
-  Just (Ann (Ctr k') scheme) | k == k' -> instantiate ops ctx scheme
+  Just (Ann (Ctr k') t) | k == k' -> Right (solve ops ctx t, ctx)
   Just sym -> Left (InvalidCtr k sym)
   Nothing -> Left (UndefinedCtr k)
 inferType ops ctx (Var x) = case lookup x ctx of
-  Just (Val (Var x')) | x == x' -> do
-    let xT = newName (x ++ "T") (map fst ctx)
-    Right (Var xT, (x, Ann (Var x) (For [xT] (Var xT))) : ctx)
+  Just (Val (Var x')) | x == x' -> Right (Typ, ctx)
   Just (Val a) -> inferType ops ctx a
-  Just (Ann (Var x') scheme) | x == x' -> instantiate ops ctx scheme
-  Just (Ann a scheme) -> do
-    (t, ctx) <- instantiate ops ctx scheme
+  Just (Ann (Var x') t) | x == x' -> Right (solve ops ctx t, ctx)
+  Just (Ann a t) -> do
     ctx <- checkType ops ctx a t
     Right (solve ops ctx t, ctx)
   Nothing -> Left (UndefinedVar x)
 inferType ops ctx (Let env a) = inferType ops (ctx ++ map (second Val) env) a
+inferType ops ctx (For x a) = do
+  let xT = newName (x ++ "T") (map fst ctx)
+  (t, ctx) <- inferType ops ((xT, Ann (Var xT) Typ) : (x, Ann (Var x) (Var xT)) : ctx) a
+  case (t, pop x ctx) of
+    (Var xT', ctx) | xT == xT' -> Right (For xT $ Var xT, ctx)
+    (t, ctx) -> Right (t, ctx)
 inferType ops ctx (Lam x a) = do
-  let xT = x ++ "T"
-  (t2, ctx) <- inferType ops ((x, Ann (Var x) (For [xT] (Var xT))) : ctx) a
-  Right (Fun (solve ops ctx (Var xT)) t2, ctx)
+  let xT = newName (x ++ "T") (map fst ctx)
+  (t2, ctx) <- inferType ops ((xT, Ann (Var xT) Typ) : (x, Ann (Var x) (Var xT)) : ctx) a
+  case (solve ops ctx (Var xT), pop x ctx) of
+    (t1, ctx) | xT `occurs` t1 -> Right (For xT $ Fun t1 t2, ctx)
+    (t1, ctx) -> Right (Fun t1 t2, ctx)
+inferType ops ctx (Case k a) = do
+  (altT, ctx) <- inferType ops ctx (Ctr k)
+  (branchT, ctx) <- inferType ops ctx a
+  inferCaseType ops ctx altT branchT
 inferType ops ctx (Fun a b) = do
   ctx <- checkType ops ctx a Typ
   ctx <- checkType ops ctx b Typ
   Right (Typ, ctx)
+inferType ops ctx (Or a b) = do
+  (t, ctx) <- inferType ops ctx a
+  ctx <- checkType ops ctx b t
+  Right (solve ops ctx t, ctx)
 inferType ops ctx (App a b) = do
   (ta, ctx) <- inferType ops ctx a
-  case ta of
-    Fun t1 t2 -> do
+  case instantiate ops ctx ta of
+    (Fun t1 t2, ctx) -> do
       ctx <- checkType ops ctx b t1
       Right (solve ops ctx t2, ctx)
-    ta -> Left (NotAFunction a ta)
-inferType _ _ (Case []) = Left EmptyCase
-inferType ops ctx (Case [(alt, branch)]) = do
-  (altT, ctx) <- inferType ops ctx (Ctr alt)
-  (branchT, ctx) <- inferType ops ctx branch
-  inferTypeCase ops ctx altT branchT
-inferType ops ctx (Case (case' : cases)) = do
-  (t, ctx) <- inferType ops ctx (Case [case'])
-  (t', ctx) <- inferType ops ctx (Case cases)
-  ctx <- unify ops ctx t t'
-  Right (solve ops ctx t, ctx)
+    (ta, _) -> Left (NotAFunction a ta)
 inferType ops ctx (Op op args) = case lookup op ctx of
   Just (Ann (Var op') scheme) | op == op' -> do
     (t, ctx) <- inferType ops ((op, Ann (Var op) scheme) : ctx) (app (Var op) args)
     Right (t, pop op ctx)
   Just sym -> Left (InvalidOp op sym)
   Nothing -> Left (UndefinedOp op)
+inferType _ _ Err = Left RuntimeError
 
-inferTypeCase :: Ops -> Context -> Type -> Type -> Either TypeError (Type, Context)
-inferTypeCase ops ctx (Fun alt1 alt2) (Fun br1 br2) = do
+inferCaseType :: Ops -> Context -> Type -> Type -> Either TypeError (Type, Context)
+inferCaseType ops ctx (For x a) b = do
+  let xT = newName (x ++ "T") (map fst ctx)
+  (t, ctx) <- inferCaseType ops ((xT, Ann (Var xT) Typ) : (x, Ann (Var x) (Var xT)) : ctx) a b
+  case (t, pop x ctx) of
+    (t, ctx) | x `occurs` t -> Right (For x t, ctx)
+    (t, ctx) -> Right (t, ctx)
+inferCaseType ops ctx a (For x b) = do
+  let xT = newName (x ++ "T") (map fst ctx)
+  (t, ctx) <- inferCaseType ops ((xT, Ann (Var xT) Typ) : (x, Ann (Var x) (Var xT)) : ctx) a b
+  Right (t, pop x ctx)
+inferCaseType ops ctx (Fun alt1 alt2) (Fun br1 br2) = do
   ctx <- unify ops ctx br1 alt1
-  inferTypeCase ops ctx alt2 br2
-inferTypeCase ops ctx altT branchT = Right (solve ops ctx (Fun altT branchT), ctx)
+  inferCaseType ops ctx alt2 br2
+inferCaseType ops ctx altT branchT = Right (solve ops ctx (Fun altT branchT), ctx)
 
 checkType :: Ops -> Context -> Expr -> Type -> Either TypeError Context
 checkType _ ctx (Op _ []) _ = Right ctx
--- test/CoreTests.hs
+++ test/CoreTests.hs
@@ -24,39 +24,44 @@ coreTests = describe "--==☯️ Core language ☯️==--" $ do
       mul [Int a, Int b] = Just (Int (a * b))
       mul _ = Nothing
 
+  -- it "☯ eval" $ do
+  -- it "☯ occurs" $ do
+  -- it "☯ unify" $ do
+
   it "☯ instantiate" $ do
-    let (a, a1) = (Var "a", Var "a1")
+    let (a, a1, b) = (Var "a", Var "a1", Var "b")
     let ctx :: Context
         ctx = [("a", Val IntT)]
     let inst = instantiate ops ctx
-    inst (For [] a) `shouldBe` Right (IntT, ctx)
-    inst (For ["a"] a) `shouldBe` Right (a1, ("a1", Val a1) : ctx)
+    inst a `shouldBe` (IntT, ctx)
+    inst (For "a" a) `shouldBe` (a1, ("a1", Val a1) : ctx)
+    inst (For "b" b) `shouldBe` (b, ("b", Val b) : ctx)
 
   it "☯ inferType" $ do
     let a = Var "a"
     let (x, y) = (Var "x", Var "y")
-    let (xT, x1) = (Var "xT", Var "x1")
+    let xT = Var "xT"
     let (f, g) = (Var "f", Var "g")
     let ctx :: Context
         ctx =
           [ ("Invalid", Val Typ),
-            ("Bool", Ann (Ctr "Bool") (For [] Typ)),
-            ("True", Ann (Ctr "True") (For [] $ Var "Bool")),
-            ("False", Ann (Ctr "False") (For [] $ Var "Bool")),
-            ("Maybe", Ann (Ctr "Maybe") (For [] $ Fun Typ Typ)),
-            ("Just", Ann (Ctr "Just") (For ["a"] $ Fun a (App (Var "Maybe") a))),
-            ("Nothing", Ann (Ctr "Nothing") (For ["a"] $ App (Var "Maybe") a)),
+            ("Bool", Ann (Ctr "Bool") Typ),
+            ("True", Ann (Ctr "True") (Var "Bool")),
+            ("False", Ann (Ctr "False") (Var "Bool")),
+            ("Maybe", Ann (Ctr "Maybe") (Fun Typ Typ)),
+            ("Just", Ann (Ctr "Just") (For "a" $ Fun a (App (Var "Maybe") a))),
+            ("Nothing", Ann (Ctr "Nothing") (For "a" $ App (Var "Maybe") a)),
             ("inferred", Val (Int 1)),
-            ("mismatch", Ann (Int 1) (For [] NumT)),
-            ("match", Ann (Int 1) (For [] IntT)),
-            ("typed", Ann (Var "typed") (For [] IntT)),
+            ("mismatch", Ann (Int 1) NumT),
+            ("match", Ann (Int 1) IntT),
+            ("typed", Ann (Var "typed") IntT),
             ("free", Val (Var "free")),
             ("x", Val (Int 1)),
             ("y", Val (Num 1.1)),
-            ("f", Ann f (For [] $ Fun IntT NumT)),
-            ("g", Ann g (For ["x"] $ Fun x x)),
-            ("op0", Ann (Var "op0") (For [] IntT)),
-            ("op1", Ann (Var "op1") (For [] $ Fun IntT NumT))
+            ("f", Ann f (Fun IntT NumT)),
+            ("g", Ann g (For "a" $ Fun a a)),
+            ("op0", Ann (Var "op0") IntT),
+            ("op1", Ann (Var "op1") (Fun IntT NumT))
           ]
 
     let infer = inferType ops ctx
@@ -74,90 +79,72 @@ coreTests = describe "--==☯️ Core language ☯️==--" $ do
     infer (Var "mismatch") `shouldBe` Left (Mismatch IntT NumT)
     infer (Var "match") `shouldBe` Right (IntT, ctx)
     infer (Var "typed") `shouldBe` Right (IntT, ctx)
-    infer (Var "free") `shouldBe` Right (Var "freeT", ("free", Ann (Var "free") (For ["freeT"] (Var "freeT"))) : ctx)
+    -- infer (Var "free") `shouldBe` Right (Var "freeT", ("free", Ann (Var "free") (For "freeT" (Var "freeT"))) : ctx)
     infer (Var "x") `shouldBe` Right (IntT, ctx)
     infer (Var "y") `shouldBe` Right (NumT, ctx)
     infer (Var "f") `shouldBe` Right (Fun IntT NumT, ctx)
-    infer (Var "g") `shouldBe` Right (Fun x1 x1, ("x1", Val x1) : ctx)
-    infer (Lam "x" x) `shouldBe` Right (Fun xT xT, ("xT", Val xT) : ("x", Ann x (For ["xT"] xT)) : ctx)
-    infer (Case []) `shouldBe` Left EmptyCase
-    infer (Case [("True", Int 1)]) `shouldBe` Right (Fun (Ctr "Bool") IntT, ctx)
-    infer (Case [("True", Int 1), ("False", Int 2)]) `shouldBe` Right (Fun (Ctr "Bool") IntT, ctx)
-    infer (Case [("True", Int 1), ("False", Num 1.1)]) `shouldBe` Left (Mismatch IntT NumT)
-    infer (Case [("Just", Lam "x" x)]) `shouldBe` Right (Fun (App (Ctr "Maybe") a) a, ("xT", Val a) : ("x", Ann x (For ["xT"] xT)) : ("a", Val a) : ctx)
-    infer (Case [("Just", Lam "x" x), ("Nothing", Int 0)]) `shouldBe` Right (Fun (App (Ctr "Maybe") IntT) IntT, ("a1", Val IntT) : ("xT", Val IntT) : ("x", Ann x (For ["xT"] xT)) : ("a", Val IntT) : ctx)
+    infer (Var "g") `shouldBe` Right (For "a" $ Fun a a, ctx)
+    -- TODO: Let
+    infer (For "x" (Int 1)) `shouldBe` Right (IntT, ctx)
+    infer (For "x" x) `shouldBe` Right (For "xT" xT, ctx)
+    infer (Lam "x" x) `shouldBe` Right (For "xT" $ Fun xT xT, ctx)
+    infer (Case "True" (Int 1)) `shouldBe` Right (Fun (Ctr "Bool") IntT, ctx)
+    infer (Case "Nothing" (Int 0)) `shouldBe` Right (For "a" $ Fun (App (Ctr "Maybe") a) IntT, ctx)
+    infer (Case "Just" (Lam "x" x)) `shouldBe` Right (For "a" $ Fun (App (Ctr "Maybe") a) a, ctx)
     infer (Fun (Int 1) (Num 1.1)) `shouldBe` Left (Mismatch IntT Typ)
     infer (Fun IntT (Num 1.1)) `shouldBe` Left (Mismatch NumT Typ)
     infer (Fun IntT NumT) `shouldBe` Right (Typ, ctx)
     infer (App x y) `shouldBe` Left (NotAFunction x IntT)
     infer (App f y) `shouldBe` Left (Mismatch NumT IntT)
     infer (App f x) `shouldBe` Right (NumT, ctx)
-    infer (App g x) `shouldBe` Right (IntT, ("x1", Val IntT) : ctx)
+    infer (App g x) `shouldBe` Right (IntT, ("a", Val IntT) : ctx)
     infer (Op "op0" []) `shouldBe` Right (IntT, ctx)
     infer (Op "op0" [Num 1.1]) `shouldBe` Left (NotAFunction (Var "op0") IntT)
     infer (Op "op1" [Num 1.1]) `shouldBe` Left (Mismatch NumT IntT)
     infer (Op "op1" [Int 1]) `shouldBe` Right (NumT, ctx)
 
   it "☯ Bool" $ do
+    let f = Var "f"
     let ctx :: Context
         ctx =
-          [ -- Bool = True | False
-            ("Bool", Ann (Ctr "Bool") (For [] Typ)),
-            ("True", Ann (Ctr "True") (For [] (Var "Bool"))),
-            ("False", Ann (Ctr "False") (For [] (Var "Bool")))
-            -- f True = 1
-            -- f False = 0
-            -- ("f", Val (Case [("True", Int 1), ("False", Int 0)]))
+          [ ("Bool", Ann (Ctr "Bool") Typ),
+            ("False", Ann (Ctr "False") (Var "Bool")),
+            ("True", Ann (Ctr "True") (Var "Bool")),
+            ("f", Val (Case "False" (Int 0) `Or` Case "True" (Int 1)))
           ]
 
     let infer = inferType ops ctx
     infer (Var "Bool") `shouldBe` Right (Typ, ctx)
-    infer (Var "True") `shouldBe` Right (Ctr "Bool", ctx)
     infer (Var "False") `shouldBe` Right (Ctr "Bool", ctx)
+    infer (Var "True") `shouldBe` Right (Ctr "Bool", ctx)
 
     let eval' = eval ops (ctxEnv ctx)
-    eval' (Var "Bool") `shouldBe` Ctr "Bool"
-    eval' (Var "True") `shouldBe` Ctr "True"
-    eval' (Var "False") `shouldBe` Ctr "False"
+    eval' (App f $ Var "False") `shouldBe` Int 0
+    eval' (App f $ Var "True") `shouldBe` Int 1
+    eval' (App f $ Var "y") `shouldBe` App (Case "False" (Int 0)) (Var "y") `Or` App (Case "True" (Int 1)) (Var "y")
 
   it "☯ Maybe" $ do
-    {- Maybe a = Just a | Nothing
+    let (f, a) = (Var "f", Var "a")
+    let ctx :: Context
+        ctx =
+          [ ("Maybe", Ann (Ctr "Maybe") (Fun Typ Typ)),
+            ("Just", Ann (Ctr "Just") (For "a" $ Fun a (App (Var "Maybe") a))),
+            ("Nothing", Ann (Ctr "Nothing") (For "a" $ App (Var "Maybe") a)),
+            ("f", Val (Case "Nothing" (Int 0) `Or` Case "Just" (Lam "x" $ Var "x")))
+          ]
 
-    Maybe : (a : Typ) -> Type
-    Maybe a = #Maybe a
+    let infer = inferType ops ctx
+    infer (Var "Maybe") `shouldBe` Right (Fun Typ Typ, ctx)
+    infer (Var "Nothing") `shouldBe` Right (For "a" $ App (Ctr "Maybe") a, ctx)
+    infer (Var "Just") `shouldBe` Right (For "a" $ Fun a (App (Ctr "Maybe") a), ctx)
+    infer (App (Var "Maybe") IntT) `shouldBe` Right (Typ, ctx)
+    infer (App (Var "Just") (Int 1)) `shouldBe` Right (App (Ctr "Maybe") IntT, ("a", Val IntT) : ctx)
+    infer (App (Var "Nothing") IntT) `shouldBe` Left (NotAFunction (Var "Nothing") (App (Ctr "Maybe") a))
 
-    Just : (a : Typ) -> (x : a) -> Maybe a
-    Just a x = #Just x
-
-    Nothing : (a : Typ) -> Maybe a
-    Nothing a = #Nothing
-
-    f Nothing = 0
-    f (Just x) = x
-
-    f : Maybe Int -> Int
-    f = @case {Just -> \x -> x | Nothing -> 0}
-    -}
-
-    -- let a = Var "a"
-    -- let env =
-    --       [ ("Maybe", Lam "a" (App (Ctr "Maybe") a))
-    --       ]
-    -- let ctx =
-    --       [ ("Maybe", Fun ("a", Typ) Typ),
-    --         ("Just", Fun ("a", Typ) $ App (Ctr "Maybe") a),
-    --         ("Nothing", Fun ("a", Typ) $ App (Ctr "Maybe") a),
-    --         ("f", Fun ("", App (Ctr "Maybe") IntT) IntT)
-    --       ]
-
-    -- let infer = inferType ops env ctx
-    -- infer (Var "Maybe") `shouldBe` Right (Fun ("a", Typ) Typ)
-    -- infer (Var "Just") `shouldBe` Right (Fun ("a", Typ) $ App (Ctr "Maybe") a)
-    -- infer (Var "Nothing") `shouldBe` Right (Fun ("a", Typ) $ App (Ctr "Maybe") a)
-    -- infer (App (Var "Maybe") IntT) `shouldBe` Right Typ
-    -- infer (App (Var "Just") IntT) `shouldBe` Right (App (Ctr "Maybe") IntT)
-    -- infer (App (Var "Nothing") IntT) `shouldBe` Right (App (Ctr "Maybe") IntT)
-    True `shouldBe` True
+    let eval' = eval ops (ctxEnv ctx)
+    eval' (App f $ Var "Nothing") `shouldBe` Int 0
+    eval' (App f $ App (Var "Just") (Int 1)) `shouldBe` Int 1
+    eval' (App f $ Var "y") `shouldBe` App (Case "Nothing" (Int 0)) (Var "y") `Or` App (Case "Just" (Lam "x" $ Var "x")) (Var "y")
 
   it "☯ factorial" $ do
     -- fact = fix (Nat->Nat) (\(f:Nat->Nat)(n:Nat).ifz n then succ 0 else mul n (f (pred n)))
