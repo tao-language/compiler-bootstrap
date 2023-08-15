dcaa970db75a7baa2624b951df8ee8af368056ae
David Cavazos <dcavazos@google.com>
2023-08-15 12:42:18 -0700

    infer Ctr, Typ, Var, Fun

--- test/NewCoreTests.hs
+++ test/NewCoreTests.hs
@@ -9,9 +9,9 @@ data Expr
   = Knd
   | IntT
   | Int !Int
-  | Var !String
   | Ctr !String
   | Typ !String
+  | Var !String
   | Fun !Expr !Expr
   | Lam !Pattern !Expr
   | App !Expr !Expr
@@ -27,11 +27,11 @@ data Pattern
   | KndP
   | IntTP
   | IntP !Int
-  | VarP !String
   | CtrP !String
   | TypP !String
-  | AppP !Pattern !Pattern
+  | VarP !String
   | FunP !Pattern !Pattern
+  | AppP !Pattern !Pattern
   | ErrP
   deriving (Eq, Show)
 
@@ -54,6 +54,8 @@ data TypeError
   = UndefinedVar !String
   | UndefinedCtr !String
   | UndefinedTyp !String
+  | InconsistentCtr !String !String
+  | InconsistentTyp !String !String
   | MissingType !String
   deriving (Eq, Show)
 
@@ -146,18 +148,33 @@ isClosed = null . freeVars
 isOpen :: Expr -> Bool
 isOpen = not . isClosed
 
+instantiate :: [String] -> Type -> (Expr, Substitution)
+instantiate _ (For [] a) = (a, [])
+instantiate existing (For (x : xs) a) = do
+  let y = freshName existing x
+  let (b, s) = instantiate (y : existing) (For xs a)
+  (eval [(x, Var y)] b, (y, Var y) : s)
+
+apply :: Substitution -> Env -> Env
+apply _ [] = []
+apply s ((x, Ann a (For xs b)) : env) = (x, Ann a (For xs (eval s b))) : apply s env
+apply s (def : env) = def : apply s env
+
+compose :: Substitution -> Substitution -> Substitution
+compose s1 s2 = apply s1 s2 `union` s1
+
 -- Evaluation
 eval :: Env -> Expr -> Expr
 eval _ Knd = Knd
 eval _ IntT = IntT
 eval _ (Int i) = Int i
+eval _ (Ctr k) = Ctr k
+eval _ (Typ t) = Typ t
 eval env (Var x) = case lookup x env of
   Just (Var x') | x == x' -> Var x
   Just (Ann (Var x') _) | x == x' -> Var x
   Just a -> eval env a
   Nothing -> Var x
-eval _ (Ctr k) = Ctr k
-eval _ (Typ t) = Typ t
 eval env (Fun a b) = Fun (eval env a) (eval env b)
 eval env (Lam p b) = Lam p (eval (pushVars (freeVarsP p) env) b)
 eval env (App a b) = case (eval env a, eval env b) of
@@ -174,9 +191,9 @@ eval env (App a b) = case (eval env a, eval env b) of
   (Or a1 a2, b) -> case eval [] (App a1 b) of
     Err -> eval [] (App a2 b)
     c -> c
-  (a@Var {}, b) -> App a b
   (a@Ctr {}, b) -> App a b
   (a@Typ {}, b) -> App a b
+  (a@Var {}, b) -> App a b
   (a@App {}, b) -> App a b
   (a, b) | isOpen b -> App a b
   (Fix x a, b) -> eval [(x, Fix x a)] (App a b)
@@ -200,54 +217,57 @@ infer :: Env -> Expr -> Either TypeError (Expr, Substitution)
 infer _ Knd = Right (Knd, [])
 infer _ IntT = Right (Knd, [])
 infer _ (Int _) = Right (IntT, [])
+infer env (Ctr k) = case lookup k env of
+  Just (Ann (Ctr k') ty) | k == k' -> do
+    let (t, s) = instantiate (map fst env) ty
+    Right (eval (apply s env) t, s)
+  Just (Ann (Ctr k') _) -> Left (InconsistentCtr k k')
+  Just _ -> Left (MissingType k)
+  Nothing -> Left (UndefinedCtr k)
+infer env (Typ t) = case lookup t env of
+  Just (Ann (Typ t') ty) | t == t' -> do
+    let (t, s) = instantiate (map fst env) ty
+    Right (eval (apply s env) t, s)
+  Just (Ann (Typ t') _) -> Left (InconsistentTyp t t')
+  Just _ -> Left (MissingType t)
+  Nothing -> Left (UndefinedTyp t)
 infer env (Var x) = case lookup x env of
-  Just (Var x') | x == x' -> error "TODO: infer Var: free variable"
+  Just (Var x') | x == x' -> do
+    let xT = freshName (map fst env) (x ++ "T")
+    Right (Var xT, [(xT, Var xT), (x, Ann (Var x) (For [] (Var xT)))])
   Just (Ann (Var x') ty) | x == x' -> do
-    let (xs, t) = instantiate (map fst env) ty
-    Right (eval (pushVars xs env) t, [])
+    let (t, s) = instantiate (map fst env) ty
+    Right (eval (apply s env) t, s)
   Just a -> infer env a
   Nothing -> Left (UndefinedVar x)
--- infer env (Ctr k args) = do
---   ctrType <- case lookup k env of
---     Just (Ann a ty) -> do
---       -- TODO: check that `a`
---       _
---     Just _ -> Left (MissingType k)
---     Nothing -> Left (UndefinedCtr k)
---   error "TODO: infer Ctr"
--- Ctr !String ![Expr]
--- Typ !String ![Expr]
--- Fun !Expr !Expr
+infer env (Fun a b) = do
+  (_, s) <- infer2 env a b
+  Right (Knd, s)
 -- Lam !Pattern !Expr
 -- App !Expr !Expr
 -- Ann !Expr !Type
 -- Or !Expr !Expr
 -- Fix !String !Expr
 -- Op2 !BinaryOp !Expr !Expr
--- Err
-infer env a = error "TODO"
+infer _ Err = Right (Err, [])
+infer _ _ = error "TODO"
 
-inferApp :: Env -> (String, Type) -> [Expr] -> Either TypeError (Expr, Substitution)
-inferApp env (x, ty) args = do
-  let y = freshName (map fst env) x
-  let env' = (y, Ann (Var y) ty) : env
-  infer env' (app (Var y) args)
+infer2 :: Env -> Expr -> Expr -> Either TypeError ((Expr, Expr), Substitution)
+infer2 env a b = do
+  (ta, s1) <- infer env a
+  (tb, s2) <- infer (apply s1 env) b
+  Right ((eval s2 ta, tb), s2 `compose` s1)
 
-apply :: Substitution -> Env -> Env
-apply _ [] = []
-apply s ((x, Ann a (For xs b)) : env) = (x, Ann a (For xs (eval s b))) : apply s env
-apply s (def : env) = def : apply s env
-
-instantiate :: [String] -> Type -> ([String], Expr)
-instantiate _ (For [] a) = ([], a)
-instantiate existing (For (x : xs) a) = do
-  let y = freshName existing x
-  let (ys, b) = instantiate (y : existing) (For xs a)
-  (y : ys, b)
+-- inferApp :: Env -> (String, Type) -> [Expr] -> Either TypeError (Expr, Substitution)
+-- inferApp env (x, ty) args = do
+--   let y = freshName (map fst env) x
+--   let env' = (y, Ann (Var y) ty) : env
+--   infer env' (app (Var y) args)
 
 run :: SpecWith ()
 run = describe "--==☯️ Core language ☯️==--" $ do
   let (i1, i2, i3) = (Int 1, Int 2, Int 3)
+  let (a, b, c) = (Var "a", Var "b", Var "c")
   let (x, y, z) = (Var "x", Var "y", Var "z")
   let (f, g, h) = (Var "f", Var "g", Var "h")
 
@@ -270,18 +290,10 @@ run = describe "--==☯️ Core language ☯️==--" $ do
 
   describe "☯ eval core" $ do
     it "☯ eval const" $ do
-      let env = []
-      eval env Knd `shouldBe` Knd
-      eval env IntT `shouldBe` IntT
-      eval env (Int 1) `shouldBe` Int 1
-
-    it "☯ eval Var" $ do
-      let env = [("x", Int 1), ("b", Int 2), ("a", Var "b"), ("c", Ann (Var "c") (For [] (Var "t")))]
-      eval env (Var "x") `shouldBe` Int 1
-      eval env (Var "y") `shouldBe` Var "y"
-      eval env (Var "z") `shouldBe` Var "z"
-      eval env (Var "a") `shouldBe` Int 2
-      eval env (Var "c") `shouldBe` Var "c"
+      eval [] Knd `shouldBe` Knd
+      eval [] IntT `shouldBe` IntT
+      eval [] (Int 1) `shouldBe` Int 1
+      eval [] Err `shouldBe` Err
 
     it "☯ eval Ctr" $ do
       let env = [("x", Int 1)]
@@ -292,6 +304,14 @@ run = describe "--==☯️ Core language ☯️==--" $ do
       let env = [("x", Int 1)]
       eval env (Typ "T") `shouldBe` Typ "T"
       eval env (App (Typ "U") x) `shouldBe` App (Typ "U") (Int 1)
+
+    it "☯ eval Var" $ do
+      let env = [("x", i1), ("y", y), ("b", i2), ("a", b), ("c", Ann c (For ["a"] a))]
+      eval env (Var "x") `shouldBe` Int 1
+      eval env (Var "y") `shouldBe` Var "y"
+      eval env (Var "z") `shouldBe` Var "z"
+      eval env (Var "a") `shouldBe` Int 2
+      eval env (Var "c") `shouldBe` Var "c"
 
     it "☯ eval Fun" $ do
       let env = [("x", Int 1), ("y", IntT)]
@@ -354,9 +374,6 @@ run = describe "--==☯️ Core language ☯️==--" $ do
       eval env (sub i1 i2) `shouldBe` Int (-1)
       eval env (mul i1 i2) `shouldBe` Int 2
 
-    it "☯ eval Err" $ do
-      eval [] Err `shouldBe` Err
-
   it "☯ eval factorial" $ do
     let env = [("f", factorial "f")]
     eval env (Var "f") `shouldBe` factorial "f"
@@ -368,8 +385,78 @@ run = describe "--==☯️ Core language ☯️==--" $ do
     eval env (App f (Int 4)) `shouldBe` Int 24
     eval env (App f (Int 5)) `shouldBe` Int 120
 
-  it "☯ infer core" $ do
-    True `shouldBe` True
+  describe "☯ infer core" $ do
+    it "☯ infer const" $ do
+      infer [] Knd `shouldBe` Right (Knd, [])
+      infer [] IntT `shouldBe` Right (Knd, [])
+      infer [] (Int 1) `shouldBe` Right (IntT, [])
+      infer [] Err `shouldBe` Right (Err, [])
+
+    it "☯ infer Ctr" $ do
+      let env =
+            [ ("C1", Ann (Ctr "C1") (For [] a)),
+              ("C2", Ann (Ctr "C2") (For ["a"] a)),
+              ("C3", Ann (Ctr "C3") (For ["b"] b)),
+              ("C4", Ann (Ctr "C5") (For [] a)),
+              ("C5", Ctr "C5"),
+              ("b", b)
+            ]
+      infer env (Ctr "X") `shouldBe` Left (UndefinedCtr "X")
+      infer env (Ctr "C1") `shouldBe` Right (a, [])
+      infer env (Ctr "C2") `shouldBe` Right (a, [("a", a)])
+      infer env (Ctr "C3") `shouldBe` Right (Var "b1", [("b1", Var "b1")])
+      infer env (Ctr "C4") `shouldBe` Left (InconsistentCtr "C4" "C5")
+      infer env (Ctr "C5") `shouldBe` Left (MissingType "C5")
+
+    it "☯ infer Typ" $ do
+      let env =
+            [ ("T1", Ann (Typ "T1") (For [] a)),
+              ("T2", Ann (Typ "T2") (For ["a"] a)),
+              ("T3", Ann (Typ "T3") (For ["b"] b)),
+              ("T4", Ann (Typ "T5") (For [] a)),
+              ("T5", Typ "T5"),
+              ("b", b)
+            ]
+      infer env (Typ "X") `shouldBe` Left (UndefinedTyp "X")
+      infer env (Typ "T1") `shouldBe` Right (a, [])
+      infer env (Typ "T2") `shouldBe` Right (a, [("a", a)])
+      infer env (Typ "T3") `shouldBe` Right (Var "b1", [("b1", Var "b1")])
+      infer env (Typ "T4") `shouldBe` Left (InconsistentTyp "T4" "T5")
+      infer env (Typ "T5") `shouldBe` Left (MissingType "T5")
+
+    it "☯ infer Var" $ do
+      let (a1, yT) = (Var "a1", Var "yT")
+      let env = [("x", Int 1), ("y", y), ("b", Int 2), ("a", b), ("c", Ann c (For ["a"] a))]
+      infer env (Var "x") `shouldBe` Right (IntT, [])
+      infer env (Var "y") `shouldBe` Right (yT, [("yT", yT), ("y", Ann y (For [] yT))])
+      infer env (Var "z") `shouldBe` Left (UndefinedVar "z")
+      infer env (Var "a") `shouldBe` Right (IntT, [])
+      infer env (Var "c") `shouldBe` Right (a1, [("a1", a1)])
+
+    it "☯ infer Fun" $ do
+      let aT = Var "aT"
+      let env = [("a", a), ("b", IntT)]
+      infer env (Fun x y) `shouldBe` Left (UndefinedVar "x")
+      infer env (Fun a y) `shouldBe` Left (UndefinedVar "y")
+      infer env (Fun a b) `shouldBe` Right (Knd, [("aT", aT), ("a", Ann a (For [] aT))])
+
+    it "☯ infer Lam" $ do
+      True `shouldBe` True
+
+    it "☯ infer App" $ do
+      True `shouldBe` True
+
+    it "☯ infer Ann" $ do
+      True `shouldBe` True
+
+    it "☯ infer Or" $ do
+      True `shouldBe` True
+
+    it "☯ infer Fix" $ do
+      True `shouldBe` True
+
+    it "☯ infer Op2" $ do
+      True `shouldBe` True
 
   it "☯ Bool" $ do
     True `shouldBe` True
