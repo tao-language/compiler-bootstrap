4684d9f7e21028e9224d25236a9ae8dbe27026c4
David Cavazos <dcavazos@google.com>
2023-10-19 16:38:06 -0700

    enable all tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,21 +1,20 @@
 module Main where
 
--- import qualified CoreTests
--- import qualified ExamplesTests
--- import qualified OperatorPrecedenceTests
+import qualified CoreTests
+import qualified ExamplesTests
+import qualified OperatorPrecedenceTests
 import qualified ParserTests
--- import qualified PrettyPrintTests
--- import qualified TaoLangTests
--- import qualified TaoTests
+import qualified PrettyPrintTests
+import qualified TaoLangTests
+import qualified TaoTests
 import Test.Hspec (hspec)
 
 main :: IO ()
 main = hspec $ do
   ParserTests.run
-
--- PrettyPrintTests.run
--- CoreTests.run
--- TaoTests.run
--- TaoLangTests.run
--- OperatorPrecedenceTests.run
--- ExamplesTests.run
+  PrettyPrintTests.run
+  CoreTests.run
+  TaoTests.run
+  TaoLangTests.run
+  OperatorPrecedenceTests.run
+  ExamplesTests.run
