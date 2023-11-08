edad2c4a9a55478a51b711d03b103b9b9fb84f56
David Cavazos <dcavazos@google.com>
2023-11-08 06:55:17 -0800

    minor simplifications

--- src/PrettyPrint.hs
+++ src/PrettyPrint.hs
@@ -22,9 +22,9 @@ join y (x : xs) = x ++ y ++ join y xs
 -- Helper functions
 render :: (Int, Int) -> (String, String) -> Layout -> String
 render _ _ [] = ""
-render (w, k) (i, j) (Text "" : y) = render (w, k) (i, j) y
 render (w, _) (i, j) (Text ('\n' : s) : y) = "\n" ++ j ++ render (w, length j) (i, j) (Text s : y)
 render (w, k) (i, j) (Text (c : s) : y) = c : render (w, k + 1) (i, j) (Text s : y)
+render (w, k) (i, j) (Text "" : y) = render (w, k) (i, j) y
 render (w, k) (i, j) (Indent x : y) = render (w, k) (i, j ++ i) x ++ render (w, k) (i, j) y
 render (w, k) (i, j) (Or x _ : y) | fits (w - k) (x ++ y) = render (w, k) (i, j) (x ++ y)
 render (w, k) (i, j) (Or _ x : y) = render (w, k) (i, j) (x ++ y)
@@ -34,6 +34,6 @@ fits w _ | w < 0 = False
 fits _ [] = True
 fits _ (Text ('\n' : _) : _) = True
 fits w (Text (_ : s) : y) = fits (w - 1) (Text s : y)
-fits w (Text s : y) = fits (w - length s) y
+fits w (Text "" : y) = fits w y
 fits w (Indent x : y) = fits w (x ++ y)
 fits w (Or x _ : y) = fits w (x ++ y)
