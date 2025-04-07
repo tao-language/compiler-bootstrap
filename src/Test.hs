f91102a131e55653c4a57622a19ec7cf71e85883
David Cavazos <dcavazos@google.com>
2025-04-07 15:28:59 -0700

    simplification

--- src/Test.hs
+++ src/Test.hs
@@ -67,7 +67,7 @@ instance TestSome (FilePath, UnitTest) where
     -- error $ show (C.dropMeta $ C.eval runtimeOps (C.Let env test'))
     case C.typedOf (C.eval runtimeOps (C.Let env test')) of
       (C.Tag ":Ok", _) -> [TestPass t.filename t.pos t.name]
-      (C.And (C.Tag ":Err") _, C.And (C.Tag ":Err") (C.Err e)) -> [TestFail t.filename t.pos t.name t.expr t.expect (lift (C.Err e))]
+      (_, C.And (C.Tag ":Err") (C.Err e)) -> [TestFail t.filename t.pos t.name t.expr t.expect (lift (C.Err e))]
       (C.And (C.Tag ":Err") got, _) -> [TestFail t.filename t.pos t.name t.expr t.expect (lift got)]
       (C.Err (RuntimeError (UnhandledCase _ (C.Ann _ (C.Err e)))), _) -> [TestFail t.filename t.pos t.name t.expr t.expect (lift (C.Err e))]
       (got, _) -> [TestFail t.filename t.pos t.name t.expr t.expect (lift got)]
